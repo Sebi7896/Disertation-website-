52210a5300e35e8d24a436caec19765ca4beead8
Sebi7896 <iaxobsebastian259@gmail.com>
2025-01-18 11:44:47 +0200

    login

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -12,30 +12,27 @@ router.post('/login', async (req, res) => {
    try {
     const user = await User.findOne({ where: { email } });
     if (!user) {
-      return res.status(401).json({ message: 'Invalid email or password' });
+      return res.status(401).json({ message: 'Invalid email does not exist!' });
     }
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-      return res.status(401).json({ message: 'Invalid email or password' });
+      return res.status(401).json({ message: 'Invalid password!' });
     }
 
-    const remebertoken = req.body.remembertoken;
+    const remeberToken = req.body.rememberToken;
 
     const accessToken = generateAccessToken(user.id);
-    if(remebertoken) {
+    if(remeberToken) {
       const refreshToken = generateRefreshToken(user.id);
       await Token.create({
         user_id: user.id,
         token: refreshToken,
         expires_at: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 zile
       });
-      return res.json({ accessToken });
     }
-    
+    return res.json({ accessToken });
+  }catch (error) {return res.status(500).json({ message: 'Internal server error' });}
 
-  }catch (error) {
-  return res.status(500).json({ message: 'Internal server error' });
-}
  });
  module.exports = router;
