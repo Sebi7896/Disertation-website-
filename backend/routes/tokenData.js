5c3290c6ab4ff3d5353c085b132a08b5aa06fd1a
Sebi7896 <iaxobsebastian259@gmail.com>
2025-01-18 15:38:31 +0200

    try catch

--- backend/routes/tokenData.js
+++ backend/routes/tokenData.js
@@ -4,8 +4,13 @@ const {verifyToken}  = require('../utils/jwt.js');
 require('dotenv').config();
 
 router.post('/tokenData', async (req, res) => {
-    const token = req.body.accessToken;
-    const decoded = await verifyToken(token); 
-    return res.status(200).json({id : decoded.userId , role : decoded.role});
+    try {
+        const token = req.body.accessToken;
+        const decoded = await verifyToken(token); 
+        return res.status(200).json({id : decoded.userId , role : decoded.role});
+    }catch (error) {
+        return res.status(401).json({ message: error.message });
+    }
+
  });
  module.exports = router;
