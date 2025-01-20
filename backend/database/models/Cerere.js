530a707634fabbf399e27b663e42f1b800a1cbc2
Sebi7896 <iaxobsebastian259@gmail.com>
2025-01-20 12:39:01 +0200

    commit final cereri

--- backend/database/models/Cerere.js
+++ backend/database/models/Cerere.js
@@ -63,10 +63,6 @@ async function getIdProfesoriCereri(studentId) {
             where: { student_id: studentId },
             attributes: ['professor_id']
         });
-        const ids = cereri.map(cerere => cerere.dataValues.professor_id);
-        // if(ids[0] === null) {
-        //     return [];
-        // }
         return ids;
     } catch (error) {
         console.error('Error fetching cereri:', error);
