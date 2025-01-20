620aa721e18542db544928c54c7825f56dd99298
Sebi7896 <iaxobsebastian259@gmail.com>
2025-01-20 13:57:48 +0200

    commit de adaugare cerere preliminara

--- backend/routes/StudentRoutes/adaugaCerere.js
+++ backend/routes/StudentRoutes/adaugaCerere.js
@@ -2,31 +2,29 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../../middleware/auth.js');
 const Cerere = require('../../database/models/Cerere.js');
+const Student = require('../../database/models/Student.js');
 const e = require('express');
+const { use } = require('./chooseTeacher.js');
 //const bcrypt = require('bcryptjs');
 
 
 //adaugam o cerere preliminara dupa prof pending
 router.put('/adaugaCerere', authMiddleware, async (req, res) => {
-    const { idStudent, idCerere, idProfesor, mesaj, titlu } = req.body;
-    if (!idStudent || !idCerere || !idProfesor || !mesaj || !titlu) {
+    const { idProfesor, mesaj, titlul } = req.body;
+    const userId = req.user.userId;
+    if (!idProfesor || !mesaj || !titlul) {
         return res.status(400).json({ message: 'Toate câmpurile sunt obligatorii.' });
     }
     try {
         // Verificăm dacă cererea aparține studentului
-        const idCerereSelectat = await Cerere.getCerereDupaId(idStudent);
-
-        if (!idCerereSelectat || idCerereSelectat.id !== idCerere) {
-            return res.status(404).json({ message: ';))' });
-        }
-
+        const studentId =await Student.getStudentIdByUserId(userId);
+    
         // Inseram datele noi în baza de date
-        const inserata = await Cerere.insertTitleAndMessage(titlu, mesaj, idProfesor,idStudent);
-
+        console.log(studentId, idProfesor, mesaj, titlul);  
+        const inserata = await Cerere.insertTitleAndMessage(titlul, mesaj, idProfesor,studentId);
         if (!inserata) {
             return res.status(500).json({ message: 'Eroare la inserarea cererii.' });
         }
-
         res.status(200).json({ message: 'Cerere inserată cu succes!' });
     } catch (error) {
         console.error('Error in insert:', error);
