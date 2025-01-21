6d53f64ec8b91decefb31128e63c615d5263c675
KhaledHachem85 <KhaledHachem85@gmail.com>
2025-01-21 13:03:06 +0200

    return nume student

--- backend/routes/StudentRoutes/chooseTeacher.js
+++ backend/routes/StudentRoutes/chooseTeacher.js
@@ -13,11 +13,11 @@ const authMiddleware = require('../../middleware/auth');
 router.post('/chooseTeacher', authMiddleware, async (req, res) => {
     const user_id = req.user.userId; 
     const studentId =await Student.getStudentIdByUserId(user_id);
-
+    const student = await Student.getStudentById(user_id);
     const cereri = await Cerere.getIdProfesoriCereri(studentId); 
     //preia toti profii disponibili si care nu sunt deja alesi
     const teachers =await Student.getProfesoriDeAlesFacultateSpecializare(studentId,cereri);
-    return res.status(200).json({studentId: studentId, teachers: teachers});
+    return res.status(200).json({studentId: studentId, teachers: teachers, student: student});
 });
   
 module.exports = router;
