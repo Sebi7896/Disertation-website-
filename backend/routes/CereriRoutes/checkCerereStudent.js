3310e73f2e4d0cc5f7bdf52995d4d526ed0af867
Sebi7896 <iaxobsebastian259@gmail.com>
2025-01-22 10:03:56 +0200

    logica login student /checkCerereStudent

--- backend/routes/CereriRoutes/checkCerereStudent.js
+++ backend/routes/CereriRoutes/checkCerereStudent.js
@@ -10,16 +10,26 @@ const authMiddleware = require('../../middleware/auth');
 
 
 //pentru a alege profesorii disponibili si pe care nu i a ales deja
-router.get('/checkRequests', authMiddleware, async (req, res) => {
+router.get('/checkCerereStudent', authMiddleware, async (req, res) => {
     const user_id = req.user.userId; 
     const studentId =await Student.getStudentIdByUserId(user_id);
 
     const cereri = await Cerere.getCereriDupaId(studentId); 
-    const cerereAcceptata = cereri.find(cerere => cerere.status_acceptare_profesor === "accepted");
-    return res.status(200).json({
-        message: cerereAcceptata ? 'Cererea a fost acceptată' : 'Cererea nu a fost acceptată',
-        cerere: !!cerereAcceptata
-    });
+    console.log(cereri);
+    if(cereri.length === 1) {
+        if(cereri.at(0).status_acceptare_profesor === 'pending') {
+            res.status(200).json({route: '/HomeStudent'});
+        }else {
+            console.log(cereri.at(0).signed_by_professor)
+            if(cereri.at(0).signed_by_professor) {
+                res.status(200).json({route: '/RutaFinalaAcceptat'});
+            }else {
+                res.status(200).json({route: '/IncarcarePdf'});
+            }
+        }
+    } else {
+        res.status(200).json({route: '/HomeStudent'});
+    }   
 });
   
 module.exports = router;
